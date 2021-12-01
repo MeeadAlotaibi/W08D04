fa31b490f8b1e828265e97d086dbb16a77f261ff
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-12-02 00:53:30 +0300

    lll

--- routers/controllers/post.js
+++ routers/controllers/post.js
@@ -3,14 +3,15 @@ const postModel = require("./../../db/models/post");
 /////////////// Create New Post ///////////////////////////
 
 const createNewPost = (req, res) => {
-  const newPost = new postModel({ img ,description , isDel , user: req.token.id }); // ما راح يقدر ينشئ تاسك جديد الا اذا كان مسجل دخول و عنده توكن 
+  const { img, desc } = req.body;
+  const newPost = new postModel({ img, desc, user: req.token.id });
   newPost
     .save()
     .then((result) => {
-      res.status(201).json(result);
+      res.status(201).send(result);
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.status(400).send(err);
     });
 }; // Done
 
@@ -18,93 +19,87 @@ const createNewPost = (req, res) => {
 
 const getPosts = (req, res) => {
   postModel
-  .find({}) //اووجد الجميييع 
+    .find({ isDel: false })
     .then((result) => {
-      if (result) { // اذا كان فيه نتيجة ؟
-        res.status(200).json(result); // رجعها لي 
+      if (result.length > 0) {
+        res.status(201).send(result);
       } else {
-        res.status(404).json("There is no posts");
-     }   
- }).catch((err) => {
-      res.status(400).json(err);
+        res.status(404).send("There is no posts");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
     });
 }; //Done
 
 ///////////////// Delete Post ////////////////////////////
 
 const deletePost = async (req, res) => {
-  const id = req.params.id; // احذف تاسك  بالآيدي عن طريق البرامز
-  let user = false;
+  const { id } = req.params;
 
-  await postModel
-    .findOne({ _id: id, user: req.token.id }) 
+  postModel
+    .findOneAndUpdate(
+      { _id: id, user: req.token.id, isDel: false },
+      { isDel: true },
+      { new: true }
+    )
+    .exec()
     .then((result) => {
       if (result) {
-        user = true;   
-      }
-    }); 
-      const result = await postModel.findById(req.token.role);
-      if (result.role == "admin" || user) { //هنا نشيّك اذا اليوزر ادمن او لا 
-        postModel
-          .findByIdAndUpdate(id, { $set: { isDel : true } })
-          .then((result) => {
-            if (result) {
-              res.status(200).json("post removed");
-            } else {
-              res.status(404).json("post does not exist");
-            }
-          })
-          .catch((err) => {
-            res.status(400).json(err);
-          });
+        res.status(201).send("post is deleted");
       } else {
-        res.json("you don't have the Authorization to delete the post");
+        res.status(404).send("Post does not exist");
       }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+
 
 };  // Done
 
 ////////////// Update Task ////////////////////////
 
-// const updatePost = (req, res) => {
-//     //ناخذ اسم التاسك و ازديليت من البدي 
-//   const name = req.body.name;
-//   const isDeleted = req.body.isDeleted; //  :) ابغى اسوي ابديت على الديليت ايضا 
-//   const id = req.params.id; // ناخذ الايدي تبع التاسك من البرامز
-//   postModel
-//     .findByIdAndUpdate(id, {
-//       $set: {
-//         name: name, //  غير لي على اسم التاسك بالاسم اللي حطيته في البدي //
-//         isDeleted: isDeleted, //  غير لي على حالة الازديليت بالحاله  اللي حطيتها له في البدي //
-//       },
-//     })
-//     .then((result) => {
-//       if (result) { // يشيك على النتيجة 
-//         res.status(200).json("Task is updated"); //اذا فيه نتيجة اظهر هذا المسج 
-//       } else {
-//         res.status(404).json("Task has not been found");// اذا مافيه
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// }; // I'm Not Finish Yet :(
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { img, desc } = req.body;
+
+  postModel
+    .findOneAndUpdate(
+      { _id: id, user: req.token.id },
+      { img, desc },
+      { new: true }
+    )
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.status(201).send("Post is updated");
+      } else {
+        res.status(404).send("Post does not exist");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+}; // done
 
 
 /////////////// Get Post By Id ///////////////////////
 
 const getPostById = (req, res) => {
-  const { id } = req.params; // اجيب كل التاسكات  بالآيدي عن طريق البرامز
+  const { id } = req.params;
   postModel
-    .find({ _id: id }) 
+    .findById(id)
+    .exec()
     .then((result) => {
       if (result) {
-        res.status(200).json(result);
+        res.status(201).send(result);
       } else {
-        res.status(404).json("Post does not exist");
+        res.status(404).send("post is not exist");
       }
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.status(400).send(err);
     });
 }; /// Done 
 
@@ -113,6 +108,6 @@ module.exports = {
   createNewPost,
   getPosts,
   deletePost,
-  // updatePost,
+  updatePost,
   getPostById,
 };
--- routers/routes/post.js
+++ routers/routes/post.js
@@ -3,20 +3,22 @@ const {
   createNewPost,
   getPosts,
   deletePost,
-  // updatePost,
-  // getPostById,
+  updatePost,
+  getPostById,
 } = require("./../controllers/post");
 const postRouter = express.Router();
 const authentication = require("../middlewares/authentication");
+const authorization = require("./../middlewares/authorization");
+
 
 
 //////////////////////////////////////////////
 
-postRouter.post("/post", authentication, createNewPost);
-postRouter.get("/posts", authentication, getPosts);
-postRouter.delete("/post/:id", authentication, deletePost);
-// postRouter.put("/task/:id", authentication, updateTask);
-// postRouter.get("/task/:id", authentication, getTaskById);
+postRouter.post("/newpost", authentication, createNewPost);
+postRouter.get("/getposts", authentication,authorization , getPosts);
+postRouter.delete("/deletePost/:id", authentication, deletePost);
+postRouter.put("/updatepost/:id", authentication, updatePost);
+postRouter.get("/getpost/:id", authentication, getPostById);
 
 
 module.exports = postRouter;
